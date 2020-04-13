38d49e426a5d7a8624da621c55380214f5d383bf
Alexey-og <54952300+Alexey-og@users.noreply.github.com>
2020-04-13 13:00:46 +0300

    Шаблонизация

---
+++ data.php
@@ -0,0 +1,41 @@
+<?php
+
+$posts = [
+    [
+        "title" => "Цитата",
+        "type" => "post-quote",
+        "content" => "Мы в жизни любим только раз, а после ищем лишь похожих",
+        "user" => "Лариса",
+        "avatar" => "userpic-larisa-small.jpg"
+    ],
+    [
+        "title" => "Игра престолов",
+        "type" => "post-text",
+        "content" => "Не могу дождаться начала финального сезона своего любимого сериала! Повседневная практика показывает, что постоянный количественный рост и сфера нашей активности обеспечивает широкому кругу (специалистов) участие в формировании системы обучения кадров, соответствует насущным потребностям.",
+        "user" => "Владик",
+        "avatar" => "userpic.jpg"
+    ],
+    [
+        "title" => "Наконец, обработал фотки!",
+        "type" => "post-photo",
+        "content" => "rock-medium.jpg",
+        "user" => "Виктор",
+        "avatar" => "userpic-mark.jpg"
+    ],
+    [
+        "title" => "Моя мечта",
+        "type" => "post-photo",
+        "content" => "coast-medium.jpg",
+        "user" => "Лариса",
+        "avatar" => "userpic-larisa-small.jpg"
+    ],
+    [
+        "title" => "Лучшие курсы",
+        "type" => "post-link",
+        "content" => "www.htmlacademy.ru",
+        "user" => "Владик",
+        "avatar" => "userpic.jpg"
+    ]
+];
+
+?>
